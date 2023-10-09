24ffce6da0ed1bcf28c5f7c7529f41ec0de4e952
HarivaradhanKM <harivaradhankm@gmail.com>
2023-10-09 12:46:23 +0000

    mid_update_IMT1D_01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import {useState} from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import ProtectedRoute from './ProtectedRoute'
 
 import './App.css'
 
@@ -12,34 +7,14 @@ import Login from './Login'
 import Home from './Home'
 import NotFound from './NotFound'
 
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-
-  const handleLogin = () => {
-    setIsLoggedIn(true)
-  }
-
-  const handleLogout = () => {
-    setIsLoggedIn(false)
-  }
-
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          {isLoggedIn ? (
-            <Home onLogout={handleLogout} />
-          ) : (
-            <Redirect to="/ebank/login" />
-          )}
-        </Route>
-        <Route path="/ebank/login">
-          {isLoggedIn ? <Redirect to="/" /> : <Login onLogin={handleLogin} />}
-        </Route>
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </Router>
-  )
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/ebank/login" component={Login} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <Route path="/" component={NotFound} />
+    </Switch>
+  </Router>
+)
 
 export default App
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,24 +1,33 @@
 import './index.css'
+import Cookies from 'js-cookie'
 
-const Home = ({onLogout}) => (
-  <div className="home-container">
-    <div className="logo-container">
+const Home = props => {
+  const onClickLogout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/ebank/login')
+  }
+
+  return (
+    <div className="home-container">
+      <div className="logo-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/ebank-logo-img.png"
+          alt="website logo"
+          className="website-logo"
+        />
+        <button className="logout-button" type="button" onClick={onClickLogout}>
+          Logout
+        </button>
+      </div>
+      <h1 className="home-page-heading">Your Flexibility, Our Excellence</h1>
       <img
-        src="https://assets.ccbp.in/frontend/react-js/ebank-logo-img.png"
-        alt="website logo"
-        className="website-logo"
+        src="https://assets.ccbp.in/frontend/react-js/ebank-digital-card-img.png"
+        alt="digital card"
+        className="digital-card"
       />
-      <button className="logout-button" type="button" onClick={onLogout}>
-        Logout
-      </button>
     </div>
-    <h1 className="home-page-heading">Your Flexibility, Our Excellence</h1>
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/ebank-digital-card-img.png"
-      alt="digital card"
-      className="digital-card"
-    />
-  </div>
-)
+  )
+}
 
 export default Home
